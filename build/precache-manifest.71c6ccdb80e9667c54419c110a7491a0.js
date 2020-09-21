c5c45c75f24c460aabc0dc05a56f527b2a66fc58
GentB <gent@paperclicks.net>
2020-09-21 20:06:15 +0200

    frontend

---
+++ build/precache-manifest.71c6ccdb80e9667c54419c110a7491a0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6cdedbf5a3fc570d32ba12e0c346d3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bab9c029ba067bb03f0b",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "548302dc7661b73f54c4",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bab9c029ba067bb03f0b",
+    "url": "./static/js/2.398fe57a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.398fe57a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "548302dc7661b73f54c4",
+    "url": "./static/js/main.749b7311.chunk.js"
+  },
+  {
+    "revision": "9e1fab61d12ac34ac58b",
+    "url": "./static/js/runtime-main.a4313449.js"
+  },
+  {
+    "revision": "cd971c7b4ab50f5b1fc4791ebe2e0b09",
+    "url": "./static/media/CrossIcon.cd971c7b.svg"
+  },
+  {
+    "revision": "7a969f28c637cda82029b4e48062a2a2",
+    "url": "./static/media/CrossIconHover.7a969f28.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
