61f27fee8ebbbc48414bc274ff799e46460d78e0
Gent Bajko <gent.bajko@gmail.com>
2020-10-19 15:17:46 +0200

    added playwright test

--- Venom/AsyncVenom.py
+++ Venom/AsyncVenom.py
@@ -4,13 +4,15 @@ import time
 
 import aiohttp
 
-from aioselenium import Remote, Keys
-from requests_html import HTML, AsyncHTMLSession
+from aioselenium import Remote
+from requests_html import HTML
+import pandas as pd
+from collections.abc import AsyncIterable, AsyncGenerator
 
-
+data = {'Brand': [], 'Product': [], 'Price': [], 'Discounted Price': []}
 async def scraper(url):
     async with aiohttp.ClientSession() as session:
-        capabilities = DesiredCapabilities.CHROME
+        capabilities = DesiredCapabilities.FIREFOX
 
         command_executor = 'http://localhost:4444/wd/hub'
 
@@ -18,15 +20,22 @@ async def scraper(url):
         async with remote as driver:
             await driver.set_window_size(1920, 1080)
             await driver.get(url)
-            # print('Loaded:', await driver.source())
             source = await driver.source()
             s = HTML(html=source)
-            products = s.xpath('//div[@class="astra-shop-summary-wrap"]/a/@href')
+            products = s.xpath('//div[contains(@class, "normalpackage")]//p[@class="name"]/a/@href')
+            xpaths = ['//tr[@class="prd_brand_css  xans-record-"]//td/span[contains(@style,"color")]',
+                      '//div[@class="headingArea "]/h2',
+                      '//td/span/strong[@id="span_product_price_text"]',
+                      '//span/span[@id="span_product_price_sale"]']
             for product in products:
-                await driver.get(product)
+                await driver.get('https://jolse.com/' + product)
                 src = await driver.source()
                 s = HTML(html=src)
-                print(s.xpath('//h1[@class="product_title entry-title"]/text()', first=True))
+                data['Brand'].append(s.xpath(f'{xpaths[0]}/text()', first=True))
+                print(s.xpath(f'{xpaths[3]}/text()', first=True))
+                data['Product'].append(s.xpath(f'{xpaths[1]}/text()', first=True))
+                data['Price'].append(s.xpath(f'{xpaths[2]}/text()', first=True))
+                data['Discounted Price'].append(s.xpath(f'{xpaths[3]}/text()', first=True))
 
 
 async def main(urls):
@@ -34,17 +43,12 @@ async def main(urls):
 
 
 if __name__ == "__main__":
-    s = time.perf_counter()
+    start = time.perf_counter()
 
-    urls = [
-        'https://glowyskinshop.com/shop/?v=79cba1185463',
-        'https://glowyskinshop.com/shop/page/2/?v=79cba1185463',
-        'https://glowyskinshop.com/shop/page/3/?v=79cba1185463',
-        'https://glowyskinshop.com/shop/page/4/?v=79cba1185463',
-        'https://glowyskinshop.com/shop/page/5/?v=79cba1185463',
-    ]
+    urls = pd.read_csv('Jolse 2020-10-18.csv', index_col=0)['0'].to_list()[:6]
 
-    products = asyncio.run(main(urls))
+    asyncio.run(main(urls))
+    pd.DataFrame(data).to_csv('test.csv', encoding='utf-8-sig')
 
-    elapsed = time.perf_counter() - s
+    elapsed = time.perf_counter() - start
     print(f"Executed in {elapsed:0.2f} seconds.")
