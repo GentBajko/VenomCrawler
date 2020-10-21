c863038dd3f522c48bc53cf283002c05d8141fe0
O.Gjoni <ogjoni@ymail.com>
2020-10-21 20:18:46 +0200

    UI improvments,endpoint changes

--- frontend/components/service-worker.js
+++ frontend/components/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.187aa347ca8588bcb254704cf1751cdd.js"
+  "../components/precache-manifest.f77c211109d7f206e392a0256b3f9c78.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("../components/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
