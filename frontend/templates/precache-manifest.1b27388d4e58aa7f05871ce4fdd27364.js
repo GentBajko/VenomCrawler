c21afc317993659f8d7cf610b6193c14bcd0990e
O.Gjoni <ogjoni@ymail.com>
2020-10-23 18:29:40 +0200

    Fixed static file paths

---
+++ frontend/templates/precache-manifest.1b27388d4e58aa7f05871ce4fdd27364.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aef15bb471f956dbf595e105093bc5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4134ee3a173563cb9e96",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5d1b90b7d51d86d6d097",
+    "url": "/static/css/main.538dbb0b.chunk.css"
+  },
+  {
+    "revision": "4134ee3a173563cb9e96",
+    "url": "/static/js/2.152046b6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.152046b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d1b90b7d51d86d6d097",
+    "url": "/static/js/main.004ce8c9.chunk.js"
+  },
+  {
+    "revision": "a1bdc0e4dd2e2b9e935b",
+    "url": "/static/js/runtime-main.af8e2e06.js"
+  },
+  {
+    "revision": "cd971c7b4ab50f5b1fc4791ebe2e0b09",
+    "url": "/static/media/CrossIcon.cd971c7b.svg"
+  },
+  {
+    "revision": "7a969f28c637cda82029b4e48062a2a2",
+    "url": "/static/media/CrossIconHover.7a969f28.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
